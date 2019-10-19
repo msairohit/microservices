f835391bbc0b0661903d78dbada8ff09a3052f9d
sai rohit <msrohit1919@gmail.com>
2019-10-19 21:30:25 +0530

    added webclient implementation

--- movie-catalog-service/src/main/java/com/rohit/moviecatalogservice/bean/Beans.java
+++ movie-catalog-service/src/main/java/com/rohit/moviecatalogservice/bean/Beans.java
@@ -4,13 +4,19 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class RestTemplateBean {
+public class Beans {
 
     @Bean
     @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean("WebClientBuilder")
+    public WebClient.Builder getWebClientBuilder() {
+        return WebClient.builder();
     }
 }
--- movie-catalog-service/src/main/java/com/rohit/moviecatalogservice/controller/TestController.java
+++ movie-catalog-service/src/main/java/com/rohit/moviecatalogservice/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.rohit.moviecatalogservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("/catalog")
@@ -13,6 +15,13 @@ public class TestController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Qualifier("WebClientBuilder")
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+//    @Autowired
+//    private WebClient.Builder webClientBuilder;
+
     @GetMapping("/test")
     public String testMethod() {
         return "movie catalog service";
@@ -20,7 +29,15 @@ public class TestController {
 
     @GetMapping("/movieInfoService")
     public String movieInfoService() {
-        return restTemplate.getForObject("http://movie-info-service/info/test", String.class);
+
+        String webResult = webClientBuilder.build()
+                .get()
+                .uri("http://localhost:8082/info/test")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        String restTemplateResult = restTemplate.getForObject("http://movie-info-service/info/test", String.class);
+        return webResult;
     }
 
     @GetMapping("/ratingsDataService")
